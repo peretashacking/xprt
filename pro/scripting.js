64591eb4aed63ec35fa72b541818f93b1d0a5ed8
Dino <github@dinois.fun>
2024-04-24 09:49:46 -0700

    some changes i made overnight while offline

---
+++ pro/scripting.js
@@ -0,0 +1,47 @@
+// Append user version to Pro
+var usersubsc = "Pro"
+document.getElementById("usersubsc").innerHTML = usersubsc
+
+/*
+
+SCRIPT FOR
+
+profile.html
+
+SCRIPT FOR
+
+*/      
+// username picker is in a script element on page due to bugs
+
+/*
+
+SCRIPT FOR
+
+index.html
+
+SCRIPT FOR
+
+*/
+
+
+/*
+
+SCRIPT FOR
+
+allgames.html
+
+SCRIPT FOR
+
+*/
+
+
+
+/*
+
+SCRIPT FOR
+
+game.html
+
+SCRIPT FOR
+
+*/
