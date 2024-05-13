39584fc93fe5188e577c36fded0d9df842dec175
Dino <github@dinois.fun>
2024-05-13 17:29:24 -0400

    launcher (doesn't work)

---
+++ launcher/basicauth.js
@@ -0,0 +1,14 @@
+function forgot() {
+    alert("Yeah, that seems like a skill issue. Contact a Developer.");
+   };
+
+
+   
+function login() {
+    const passkey = document.getElementById('passkey');
+
+    console.log(passkey)
+    console.log(input.getElementById('passkey').inputMode)
+   };
+
+const passkey = prompt("Enter Passkey.")
