03ae787f87ddacd1bed138591598a44c4e6b7b2f
Dino <github@dinois.fun>
2024-05-14 06:52:04 -0400

    updates made on pc

---
+++ auth.js
@@ -0,0 +1,19 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+
+    fetch('auth.json')
+        .then(response => response.json())
+        .then(users => {
+            var validUser = users.find(user => user.username === username && user.password === password);
+
+            if (validUser) {
+                window.location.href = '/basic/index.html';
+            } else {
+                alert('Invalid credentials');
+            }
+        })
+        .catch(error => console.error('Error:', error));
+});
